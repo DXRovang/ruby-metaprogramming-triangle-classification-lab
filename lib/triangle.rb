1acd778033fab0927003fb07884e3fee14aeaead
Alexander Rovang <wolf_vii@yahoo.com>
2020-12-25 21:55:39 -0500

    commit tree 4258d673e710fb0a57bc2cbc240ad210f98f3bcb

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,41 @@
+require "pry"
+
 class Triangle
-  # write code here
+
+  attr_accessor :a, :b, :c
+
+  def initialize(a, b, c)
+    @a = a 
+    @b = b 
+    @c = c   
+      #binding.pry
+  end
+
+  def valid?
+    if self.a > 0 && self.b >0 && self.c > 0
+      if (@a + @b > @c) && (@b + @c > @a) && (@c + @a > @b)
+        true
+      end
+    end
+  end
+
+  def kind
+    if valid?
+      if (self.a == self.b) && (self.b == self.c) 
+        triangle = :equilateral
+      elsif (self.a != self.b) && (self.b != self.c) && (self.c != self.a)
+        triangle =  :scalene
+      else
+        triangle = :isosceles
+      end
+      triangle
+    else
+      raise TriangleError
+    end        
+  end
+
+  class TriangleError < StandardError
+    # triangle error code
+  end
+ 
 end
